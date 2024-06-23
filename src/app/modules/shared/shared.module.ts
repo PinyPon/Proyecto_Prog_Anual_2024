a19b158384dedad468489a5ec3f0ca5841e773cf
PinyPon <tizianaperfumobouzon@gmail.com>
2024-06-23 20:11:53 -0300

    Creacion de las vistas

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './conponents/navbar/navbar.component';
+import { FooterComponent } from './conponents/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
