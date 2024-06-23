80b1af0945bada91a45268cb6ef23b2561152836
PinyPon <tizianaperfumobouzon@gmail.com>
2024-06-23 19:38:03 -0300

    Creación del componente registrar en la carpeta pages, dentro del modulo autentificacion

--- src/app/modules/autentificacion/autentificacion.module.ts
+++ src/app/modules/autentificacion/autentificacion.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AutentificacionRoutingModule } from './autentificacion-routing.module';
 import { IniciosesionComponent } from './pages/iniciosesion/iniciosesion.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 
 @NgModule({
   declarations: [
-    IniciosesionComponent
+    IniciosesionComponent,
+    RegistroComponent
   ],
   imports: [
     CommonModule,
