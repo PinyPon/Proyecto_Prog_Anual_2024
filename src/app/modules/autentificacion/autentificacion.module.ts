ea1e68dd9d65f7bd28193d2b5d3dec8243f7dcbc
PinyPon <tizianaperfumobouzon@gmail.com>
2024-06-23 19:35:30 -0300

    Creación del componente iniciosecion en la carpeta pages, dentro del modulo autentificacion

--- src/app/modules/autentificacion/autentificacion.module.ts
+++ src/app/modules/autentificacion/autentificacion.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AutentificacionRoutingModule } from './autentificacion-routing.module';
+import { IniciosesionComponent } from './pages/iniciosesion/iniciosesion.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IniciosesionComponent
+  ],
   imports: [
     CommonModule,
     AutentificacionRoutingModule
