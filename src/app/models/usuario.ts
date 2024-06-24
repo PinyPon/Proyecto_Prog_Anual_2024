132a54c19e37f9697d1c261ed3eb4c49e4d87d2e
PinyPon <tizianaperfumobouzon@gmail.com>
2024-06-24 08:35:59 -0300

    Creamos el servicio de firestore y de auth

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,9 @@
+export interface Admin {
+    uid: string | any;//any es indefinido o lo que sea
+    nombre: string;
+    apellido: string;
+    email: string;
+    rol: string;//sujeto a revision.
+    password: string;
+    permisos: boolean; //si no sirve, sacar.
+}
