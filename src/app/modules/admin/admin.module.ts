be04cf82ac5d4603778922f6a8c0c7749df47397
PinyPon <tizianaperfumobouzon@gmail.com>
2024-08-13 10:19:49 -0300

    Actualizacion de modulo admin

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Archivo de rutas del módulo
 import { AdminRoutingModule } from './admin-routing.module';
 
 
+// Vista
+import { AdminComponent } from './page/admin/admin.component';
+
+// Componentes locales
+import { TableComponent } from './components/table/table.component';
+
+// Paqueterías para formularios y formularios reactivos
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Componentes de material
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminComponent,
+    TableComponent
+  ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule
+  ],
+  exports:[
+    AdminComponent,
+    TableComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule
   ]
 })
 export class AdminModule { }
