8f69acf79dd82917b3f1861436f441f61a62fc26
PinyPon <tizianaperfumobouzon@gmail.com>
2024-09-19 14:53:56 -0300

    Enturamos los productos rmedianos y rgrandes

--- src/app/modules/producto/producto-routing.module.ts
+++ src/app/modules/producto/producto-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//Importaciones para que el "menú" nos mande a las paginas correspondientes
 
-const routes: Routes = [];
+import { RmedianosComponent } from '../producto/rmedianos/rmedianos.component';
+import { RgrandesComponent} from '../producto/rgrandes/rgrandes.component'
+
+
+
+const routes: Routes = [
+  {
+    path: "medianos", component: RmedianosComponent
+  },
+  {
+    path: "grandes", component: RgrandesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ProductoRoutingModule { }
+export class ProductoRoutingModule {
+
+
+ }
 
