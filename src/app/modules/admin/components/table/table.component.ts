e81a9520d405dad99b60be2a795d27a6bbcb28c1
PinyPon <tizianaperfumobouzon@gmail.com>
2024-09-19 11:06:51 -0300

    Puro CSS. Cambio drastico de la paleta de colores en toda la pagina

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -1,45 +1,10 @@
-// import { Component } from '@angular/core';
-// import { Producto } from 'src/app/models/producto';
-// import { CrudService } from '../../services/crud.service';
-// import { FormControl, FormGroup, Validators } from '@angular/forms';
-// import Swal from 'sweetalert2';
-
-// @Component({
-//   selector: 'app-table',
-//   templateUrl: './table.component.html',
-//   styleUrls: ['./table.component.css']
-// })
-// export class TableComponent {
-//   // Creamos colección local de productos -> la definimos como array
-//   coleccionProductos: Producto[] = [];
-
-//   productoSeleccionado!: Producto; // !<= indica que puede tomar valores vacios
-
-//   modalVisibleProducto: boolean = false
-
-//   // Definimos formulario para los productos
-//   /**
-//    * Atributos alfanuméricos (string) se inicializan con comillas simples
-//    * Atributos numéricos (number) se inicializan con cero ('0')
-//    */
-//   producto = new FormGroup({
-//     nombre: new FormControl('', Validators.required),
-//     precio: new FormControl(0, Validators.required),
-//     descripcion: new FormControl('', Validators.required),
-//     categoria: new FormControl('', Validators.required),
-//     imagen: new FormControl('', Validators.required),
-//     alt: new FormControl('', Validators.required)
-//   })
-
-
-
-
-
-
 import { Component } from '@angular/core';
 import { Producto } from 'src/app/models/producto';
 import { CrudService } from '../../services/crud.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+// Sweet alert
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-table',
@@ -74,7 +39,7 @@ export class TableComponent {
     // subscribe -> método de notificación de cambios (observable)
     this.servicioCrud.obtenerProducto().subscribe(producto => {
       this.coleccionProductos = producto;
-  
+
     })
   }
 
@@ -91,17 +56,23 @@ export class TableComponent {
         alt: this.producto.value.alt!
       }
 
-      
+
       await this.servicioCrud.crearProducto(nuevoProducto)
         .then(producto => {
-          alert("Ha agregado un nuevo producto con éxito.");
-
+          Swal.fire({
+            title: "¡Bien!",
+            text: "¡Producto cargado exitosamente!",
+            icon: "success"
+          });
           // Resetea el formulario y las casillas quedan vacías
           this.producto.reset();
         })
         .catch(error => {
-          alert("Ha ocurrido un error al cargar un producto.");
-
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "No pudimos guardar tu producto",
+          });
           this.producto.reset();
         })
     }
@@ -109,25 +80,31 @@ export class TableComponent {
 
   // ELIMINAR PRODUCTOS
   // función vinculada al modal y el botón de la tabla
-  mostrarBorrar(productoSeleccionado: Producto){
+  mostrarBorrar(productoSeleccionado: Producto) {
     this.modalVisibleProducto = true;
 
     this.productoSeleccionado = productoSeleccionado;
   }
 
-  borrarProducto(){
+  borrarProducto() {
     this.servicioCrud.eliminarProducto(this.productoSeleccionado.idProducto)
-    .then(respuesta => {
-      alert("Se ha podido eliminar con éxito.");
-    })
-    .catch(error => {
-      alert("Ha ocurrido un error al eliminar un producto: \n"+error);
-    })
+      .then(respuesta => {
+        Swal.fire({
+          text: "Producto eliminado!",
+          icon: "success"
+        });
+      })
+      .catch(error => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "No pudimos guardar tu producto",
+        });      })
   }
 
   // EDITAR PRODUCTOS
   // Se envía y llama al momento que tocamos botón "Editar" de la tabla
-  mostrarEditar(productoSeleccionado: Producto){
+  mostrarEditar(productoSeleccionado: Producto) {
     this.productoSeleccionado = productoSeleccionado;
     /*
       Toma los valores del producto seleccionado y los va a
@@ -144,7 +121,7 @@ export class TableComponent {
   }
 
   // VINCULA A BOTÓN "editarProducto" del modal de "Editar"
-  editarProducto(){
+  editarProducto() {
     let datos: Producto = {
       // Solo idProducto no se modifica por el usuario
       idProducto: this.productoSeleccionado.idProducto,
@@ -160,68 +137,18 @@ export class TableComponent {
 
     // Enviamos al método el id del producto seleccionado y los datos actualizados
     this.servicioCrud.modificarProducto(this.productoSeleccionado.idProducto, datos)
-    .then(producto => {
-      alert("El producto se ha modificado con éxito.");
-    })
-    .catch(error => {
-      alert("Hubo un problema al modificar el producto: \n"+error);
-    })
+      .then(producto => {
+        Swal.fire({
+          text: "Producto editado con exito!",
+          icon: "success"
+        });
+      })
+      .catch(error => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "No pudimos guardar tu producto",
+        });
+      })
   }
 }
-
-
-//   constructor(public servicioCrud: CrudService) { }
-
-//   ngOnInit(): void {
-//     this.servicioCrud.obtenerProducto().subscribe(producto => {
-//       this.coleccionProductos = producto;
-//     })
-//   }
-
-//   async agregarProducto() {
-//     if (this.producto.valid) {
-//       let nuevoProducto: Producto = {
-//         idProducto: '',
-//         nombre: this.producto.value.nombre!,
-//         precio: this.producto.value.precio!,
-//         descripcion: this.producto.value.descripcion!,
-//         categoria: this.producto.value.categoria!,
-//         imagen: this.producto.value.imagen!,
-//         alt: this.producto.value.alt!
-//       }
-
-//       await this.servicioCrud.crearProducto(nuevoProducto)
-//         .then(producto => {
-//           Swal.fire({
-//             title: "¡Producto cargado!",
-//             icon: "success"
-//           });
-//         })
-//         .catch(error => {
-//           Swal.fire({
-//             icon: "error",
-//             title: "Oops...",
-//             text: "No pudimos guardar tu producto",
-//           });
-//         })
-//     }
-
-//   }
-
-//   mostrarBorrar(productoSeleccionado: Producto) {
-//     this.modalVisibleProducto = true;
-
-//     this.productoSeleccionado = productoSeleccionado
-//   }
-
-
-// borrarProducto(){ 
-//   this.servicioCrud.eliminarProducto(this.productoSeleccionado.idProducto)
-//   .then(respuesta=>{
-//     alert ("no se pudo")
-//   })
-//   .catch(error=>{
-//     alert("si se pudo")
-//   })
-// }
-// }
