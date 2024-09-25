e2d0b4d4efe43bbacdbf5cec3518e5cdc3b7f45e
PinyPon <tizianaperfumobouzon@gmail.com>
2024-09-24 22:55:24 -0300

    Creación del modulo informacion, junto con sus componentes nosotros (con la informacion del Sobre Nosotros) y preguntas (con las Preguntas Frecuentes)

---
+++ src/app/modules/informacion/informacion.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InformacionRoutingModule } from './informacion-routing.module';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { PreguntasComponent } from './preguntas/preguntas.component';
+
+
+@NgModule({
+  declarations: [
+    NosotrosComponent,
+    PreguntasComponent
+  ],
+  imports: [
+    CommonModule,
+    InformacionRoutingModule
+  ]
+})
+export class InformacionModule { }
